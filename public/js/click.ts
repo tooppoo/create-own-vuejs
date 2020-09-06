ad616048855f633f7ad9ea2f033e1caa04d5dd5f
philomagi <tooppoo56@gmail.com>
2020-09-06 16:02:53 +0900

    add style

--- public/js/click.ts
+++ public/js/click.ts
@@ -1,10 +1,9 @@
-import { h } from '../../src/vnode/handler'
+import { h, mount, patch } from '../../src/vnode/handler'
 import { Children, VNode } from '../../src/vnode/type'
 import { reactive } from '../../src/reactive/reactive'
 import { watchEffect } from '../../src/reactive/effect'
 
-const render = (clickCount: Children) => {
-  return h(
+const render = (clickCount: Children) =>h(
     'div',
     { class: 'container' },
     [
@@ -12,17 +11,25 @@ const render = (clickCount: Children) => {
       h('p', null, 'clicks')
     ]
   )
-}
 
 const state = reactive({
   count: 0,
 })
 
 let previousNode: VNode | null = null
+const app = document.getElementById('app')
+if (!app) throw new Error('app not found')
+
 watchEffect(() => {
   if (!previousNode) {
+    previousNode = render(`${state.count}`)
 
+    mount(previousNode, app)
   } else {
+    const newNode = render(`${state.count}`)
 
+    patch(previousNode, newNode)
+
+    previousNode = newNode
   }
 })
