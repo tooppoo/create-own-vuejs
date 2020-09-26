f971a017f342cb018ba95c56053a5d1d02b8b000
philomagi <tooppoo56@gmail.com>
2020-09-26 19:50:48 +0900

    extend computed

--- spec/reactive/computed.spec.ts
+++ spec/reactive/computed.spec.ts
@@ -2,22 +2,35 @@ import { ref } from '../../src/reactive/ref'
 import { computed } from '../../src/reactive/computed'
 
 describe('computed', () => {
-  describe('ref', () => {
-    describe('function', () => {
-      it('最新の結果が返る', () => {
-        const count =  ref<number>(1)
-        const plusOne = computed(() => count.value + 1)
+  describe('function', () => {
+    it('最新の結果が返る', () => {
+      const count =  ref<number>(1)
+      const plusOne = computed(() => count.value + 1)
 
-        expect(plusOne.value).toBe(2)
+      expect(plusOne.value).toBe(2)
 
-        count.value += 1
+      count.value += 1
 
-        expect(plusOne.value).toBe(3)
+      expect(plusOne.value).toBe(3)
 
-        count.value = 0
+      count.value = 0
 
-        expect(plusOne.value).toBe(1)
+      expect(plusOne.value).toBe(1)
+    })
+  })
+
+  describe('object', () => {
+    it('最新の結果が返る', () => {
+      const count = ref<number>(1)
+      const sut = computed({
+        get: () => count.value + 3,
+        set: val => count.value = val - 2
       })
+
+      sut.value = 1
+
+      expect(count.value).toBe(-1)
+      expect(sut.value).toBe(2)
     })
   })
 })
--- src/reactive/computed.ts
+++ src/reactive/computed.ts
@@ -1,12 +1,22 @@
 import { RefReactive, ref, ReactivePrimitive } from './ref'
 import { watchEffect } from './effect'
 
-export const computed =
-  <T extends ReactivePrimitive>(f: () => T): Readonly<RefReactive<Readonly<T>>> => {
-    let value: T
+type Getter<T> = () => T
+interface Mutable<T> {
+  get(): T
+  set(t: T): void
+}
+type Computable<T> = Getter<T> | Mutable<T>
 
+type Computed<T> = RefReactive<Readonly<T>>
+export function computed<T extends ReactivePrimitive>(t: Getter<T>): Readonly<Computed<T>>
+export function computed<T extends ReactivePrimitive>(t: Mutable<T>): Computed<T>
+export function computed<T extends ReactivePrimitive>(t: Computable<T>): Computed<T> | Readonly<Computed<T>> {
+  let value: T
+
+  if (isGetter(t)) {
     watchEffect(() => {
-      value = f()
+      value = t()
     })
 
     return {
@@ -14,4 +24,23 @@ export const computed =
         return value
       }
     }
+  } else {
+    watchEffect(() => {
+      value = t.get()
+    })
+    const update = (v: T) => t.set(v)
+
+    return {
+      get value(): T {
+        return value
+      },
+      set value(v: T) {
+        update(v)
+      }
+    }
   }
+}
+
+function isGetter<T>(t: Computable<T>): t is Getter<T> {
+  return typeof t === 'function'
+}
